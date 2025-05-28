048665b0c7f87c33517b1f8320825baa80bc8ed6
Olivier Cecillon <olivier+git@cecillon.pro>
2025-05-29 00:27:07 +0200

    Refactoring done

--- fucking-build.php
+++ fucking-build.php
@@ -5,51 +5,212 @@ declare(strict_types=1);
 
 $motherfucking_time = -hrtime(true);
 
-function checkDep($cwd): void
-{
-    $parsedownPath = "$cwd/lib/Parsedown.php";
+class MotherfuckingGenerator {
+    private readonly array $config;
 
-    if (!file_exists($parsedownPath)) {
-        mkdirIfNotExists(dirname($parsedownPath));
-        writeIfNotExists(
-            $parsedownPath,
-            file_get_contents('https://github.com/erusev/parsedown/raw/refs/heads/master/Parsedown.php')
-        );
+    private readonly string $content_dir;
+
+    private readonly string $output_dir;
+
+    private readonly string $templates_dir;
+
+    private readonly Parsedown $parser;
+
+    private array $source_files = [];
+
+    private int $total_index_pages;
+
+    public function __construct(private readonly string|false $cwd) {
+        false === $cwd and exit("getcwd() failed: WTF did you do with your current working directory?");
+        $this->content_dir = "$this->cwd/content";
+        $this->output_dir = "$this->cwd/output";
+        $this->install();
+        $this->parser = new Parsedown();
+        $this->config = file_exists("$this->cwd/mfconfig.php") ? require $this->cwd . "/mfconfig.php" : [];
     }
-    require $parsedownPath;
+
+    private function install(): void {
+        $parsedown = "$this->cwd/lib/Parsedown.php";
+
+        if (!file_exists($parsedown)) {
+            mkdirIfNotExists($this->cwd.'/lib');
+            file_put_contents(
+                $parsedown,
+                file_get_contents('https://github.com/erusev/parsedown/raw/refs/heads/master/Parsedown.php')
+            );
+        }
+        require $parsedown;
+
+        if (!file_exists($this->content_dir)) {
+            $hello_dir = $this->content_dir . '/' . date('Y/m/d');
+            mkdir($hello_dir, 0750, true);
+            file_put_contents($hello_dir . '/hello-world.md', "# Hello world!\n\nThis is your mother fucking static website!");
+        }
+
+        if (!file_exists($this->templates_dir = $this->cwd . '/templates')) {
+            mkdir($this->templates_dir, 0750, true);
+            file_put_contents($this->templates_dir . '/layout.php', getTemplateLayout());
+            file_put_contents($this->templates_dir . '/index.php', getTemplateIndex());
+            file_put_contents($this->templates_dir . '/post.php', getTemplatePost());
+        }
+    }
+
+    public function build(): void {
+        rrmdir($this->output_dir);
+        $this->source_files = $this->getSourceFiles();
+        if (empty($this->source_files)) {
+            mkdir($this->output_dir);
+            $rendered = $this->renderTemplate('layout', ['config' => $this->config]);
+            file_put_contents("$this->output_dir/index.html", $rendered);
+            return;
+        }
+
+        $current_page = 1;
+        $count = 0;
+        $list = [];
+        foreach ($this->getMardownFiles() as $post) {
+            $list[] = $post;
+            $this->buildPostWithAssets($post, $this->output_dir . $post['url']);
+            $is_last_page = !isset($post['next']);
+            $posts_per_page = $this->config['posts_per_page'] ?? 10;
+            if (++$count === $posts_per_page || $is_last_page) {
+                $this->buildIndexPage($list, $current_page, $is_last_page);
+                $count = 0;
+                $list = [];
+                $current_page++;
+            }
+        }
+
+        $this->copyGlobalAssets();
+    }
+
+    private function buildPostWithAssets(array $post, string $post_output_dir): void {
+        $post['content'] = $this->parser->text($post['content']);
+        $rendered = $this->renderTemplate('post', ['post' => $post]);
+        $data = [
+            'body' => $rendered,
+            'config' => $this->config,
+        ];
+
+        mkdir($post_output_dir, 0750, true);
+        file_put_contents("$post_output_dir/index.html", $this->renderTemplate('layout', $data));
+
+        // File assets
+        if (is_dir($this->content_dir . $post['url'])) {
+            $assets = glob($this->content_dir . $post['url'] . '*');
+            foreach ($assets as $asset) {
+                copy($asset, $post_output_dir . '/' . basename($asset));
+            }
+        }
+    }
+
+    private function buildIndexPage(array $posts, int $page_num, $is_last_page): void {
+        $rendered = $this->renderTemplate('index', [
+            'posts' => $posts,
+            'previous_url' => index_url($page_num - 1),
+            'next_url' => $is_last_page ? null : index_url($page_num + 1),
+        ]);
+        $rendered = $this->renderTemplate('layout', [
+            'body' => $rendered,
+            'config' => $this->config,
+        ]);
+        $page_slug = index_url($page_num);
+        $output_dir = $this->output_dir . '/' . $page_slug;
+
+        mkdirIfNotExists($output_dir, 0750, true);
+        file_put_contents("$output_dir/index.html", $rendered);
+    }
+
+    private function copyGlobalAssets(): void {
+        $assets_dir = $this->content_dir . '/assets';
+        if (is_dir($assets_dir)) {
+            foreach (getIterator($assets_dir) as $asset) {
+                $target_path = "$this->output_dir/assets/" . substr($asset, strlen($assets_dir) + 1);
+                mkdirIfNotExists(dirname($target_path));
+                copy($asset, $target_path);
+            }
+        }
+    }
+
+    private function getSourceFiles(): array {
+        $source_files = [];
+        $iterator = getIterator($this->content_dir);
+        foreach ($iterator as $file) {
+            if (!str_ends_with($file, '.md')) {
+                continue;
+            }
+            $relative_path = substr($file, strlen($this->content_dir) + 1);
+            $source_files[] = [
+                'relative_path' => $relative_path,
+                'pathname' => $file,
+            ];
+        }
+        rsort($source_files);
+        $this->total_index_pages = (int) ceil(count($source_files) / ($this->config["nb_per_page"] ?? 10));
+        return $source_files;
+    }
+
+    private function getMardownFiles(): Generator {
+        $previous = null;
+        foreach ($this->source_files as $index => $file) {
+            if (!isset($file['content'])) {
+                $file = $this->enrichFileData($file);
+            }
+
+            $next = null;
+            if (isset($this->source_files[$index + 1])) {
+                $next = $source_files[$index + 1] = $this->enrichFileData($this->source_files[$index + 1]);
+            }
+
+            $file_data = $file + [
+                'previous' => $previous,
+                'next' => $next
+            ];
+            $previous = $file;
+            yield $file_data;
+        }
+    }
+
+    private function enrichFileData(array $file): array
+    {
+        $content = file_get_contents($file['pathname']);
+        return $file + [
+            'title' => $this->extractTitle($content),
+            'content' => $content,
+            'url' => '/' . substr_replace($file['relative_path'], '/', -3),
+        ];
+    }
+
+    private function extractTitle(string &$markdown): string
+    {
+        if (preg_match('`^# (.+)\R+`', ltrim($markdown), $matches)) {
+            $markdown = ltrim(substr($markdown, strlen($matches[0])));
+            return $matches[1];
+        }
+        return '';
+    }
+
+    private function renderTemplate(string $path, array $vars): string
+    {
+        $path = "$this->templates_dir/$path.php";
+        extract($vars);
+        ob_start();
+        include $path;
+        return ob_get_clean();
+    }
 }
 
-function checkContent(string $content_dir): void
-{
-    if (! file_exists($content_dir)) {
-        mkdir($content_dir, 0750);
-        $hello_path = "$content_dir/" . date('Y/m/d') . '/hello-world.md';
-        mkdirIfNotExists(dirname($hello_path));
-        writeIfNotExists($hello_path, "# Hello world!\n\nThis is your mother fucking static website!");
-    }
-}
-
-function checkTemplates(string $templates_dir): void
-{
-    if (! file_exists($templates_dir)) {
-        mkdir($templates_dir, 0750);
-        file_put_contents($templates_dir . '/layout.php', getTemplateLayout());
-        file_put_contents($templates_dir . '/index.php', getTemplateIndex());
-        file_put_contents($templates_dir . '/post.php', getTemplatePost());
-    }
+function getIterator(string $path): RecursiveIteratorIterator {
+    return new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_PATHNAME),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
 }
 
 function mkdirIfNotExists(string $dir): void
 {
     if (!file_exists($dir)) {
         mkdir($dir, 0750, true);
-    }
-}
-
-function writeIfNotExists(string $file, string $content): void
-{
-    if (! file_exists($file)) {
-        file_put_contents($file, $content);
     }
 }
 
@@ -60,12 +221,10 @@ function rrmdir(string $dir): void
     }
 
     foreach (scandir($dir) as $item) {
-        if ($item === '.' || $item === '..') {
-            continue;
-        }
-        $path = $dir . DIRECTORY_SEPARATOR . $item;
+        if ($item === '.' || $item === '..') { continue; }
+        $path = "$dir/$item";
         if (is_dir($path)) {
-            rrmdir($path); // récursivité
+            rrmdir($path);
         } else {
             unlink($path);
         }
@@ -73,100 +232,11 @@ function rrmdir(string $dir): void
     rmdir($dir);
 }
 
-function getMarkdownFiles(string $base_path): Generator
-{
-    $iterator_flags = FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_PATHNAME;
-    $iterator = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($base_path, $iterator_flags),
-        RecursiveIteratorIterator::LEAVES_ONLY
-    );
-
-    $source_files = [];
-    foreach ($iterator as $file) {
-        if (!str_ends_with($file, '.md')) {
-            continue;
-        }
-        $relative_path = substr($file, strlen($base_path) + 1);
-        $source_files[] = [
-            'relative_path' => $relative_path,
-            'pathname' => $file,
-        ];
-    }
-    rsort($source_files);
-
-    $previous = null;
-    foreach ($source_files as $index => $file) {
-        if (!isset($file['content'])) {
-            $file = complete_file_data($file);
-        }
-
-        if (isset($source_files[$index + 1])) {
-            $next = $source_files[$index + 1] = complete_file_data($source_files[$index + 1]);
-        } else {
-            $next = null;
-        }
-
-        $file_data = $file + [
-            'previous' => $previous,
-            'next' => $next
-        ];
-        $previous = $file;
-        yield $file_data;
-    }
-}
-
 function excerpt(string $html, int $limit = 50, string $suffix = '…'): string
 {
     $words = preg_split('`\s+`', strip_tags($html));
     $excerpt = implode(' ', array_slice($words, 0, $limit));
     return  $excerpt . (count($words) > $limit ? $suffix : '');
-}
-
-function complete_file_data(array $file): array
-{
-    $content = file_get_contents($file['pathname']);
-    return $file + [
-        'title' => extractTitle($content),
-        'content' => $content,
-        'url' => '/' . substr_replace($file['relative_path'], '/', -3),
-    ];
-}
-
-function extractTitle(string &$markdown): string
-{
-    if (preg_match('`^# (.+)\R+`', ltrim($markdown), $matches)) {
-        $markdown = ltrim(substr($markdown, strlen($matches[0])));
-        return $matches[1];
-    }
-    return '';
-}
-
-function checkEnv(string|false $cwd): void
-{
-    if (false === $cwd) {
-        echo "getcwd() fucking failed. What's wrong with you?";
-        exit;
-    }
-
-    if ($_SERVER['argc'] > 1) {
-        echo "You don't need fucking arguments. This script does only one shit. Deal with it.\n";
-        exit;
-    }
-}
-
-function checkConfig(string $cwd, $default_config): void
-{
-    if (!file_exists($cwd . '/mfconfig.php')) {
-        file_put_contents($cwd . '/mfconfig.php', $default_config);
-    }
-}
-
-function render_template(string $path, array $vars): string
-{
-    extract($vars);
-    ob_start();
-    include $path;
-    return ob_get_clean();
 }
 
 function index_url(int $page): ?string
@@ -178,15 +248,6 @@ function index_url(int $page): ?string
     };
 }
 
-$default_config = <<<'config'
-<?php
-
-return [
-    'site_title' => 'This is a motherfucking static blog',
-];
-
-config;
-
 function getTemplateLayout(): string
 {
     return <<<'layout'
@@ -194,14 +255,16 @@ function getTemplateLayout(): string
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><?=htmlspecialchars($config['site_title'])?></title>
+    <title><?=htmlspecialchars($config['site_title'] ?? '')?></title>
     <style>body{margin:40px auto;width: 100%;max-width:70ch;line-height:1.6;font-size:18px;color:#444;padding:0 10px}
     h1,h2,h3{line-height:1.2}</style>
 </head>
 <body>
+    <?php if(isset($config['site_title'])): ?>
     <header>
         <h1><a href="/"><?=htmlspecialchars($config['site_title'])?></a></h1>
     </header>
+    <?php endif; ?>
     <main>
         <?= isset($body) ? $body : "<p>This motherfucking static blog is fucking empty.</p>"?>
     </main>
@@ -215,11 +278,10 @@ function getTemplateLayout(): string
 layout;
 }
 
-function getTemplateIndex(): string
-{
+function getTemplateIndex(): string {
     return <<<'index'
-<?php if (!empty($list)):?>
-    <?php foreach ($list as $post):?>
+<?php if (!empty($posts)):?>
+    <?php foreach ($posts as $post):?>
         <article>
             <h1><a href="<?=$post['url']?>"><?=htmlspecialchars($post['title'])?></a></h1>
             <?=excerpt($post['content'])?>
@@ -235,10 +297,12 @@ function getTemplateIndex(): string
 index;
 }
 
-function getTemplatePost(): string
-{
+function getTemplatePost(): string {
     return <<<'post'
 <article>
+    <header>
+    <h1><?= $post['title']?></h1>
+    </header>
     <?=$post['content']?>
 </article>
 <?php if(isset($post['previous'])):?><a href="<?=$post['previous']['url']?>">&laquo; Previous</a><?php endif?>
@@ -246,95 +310,8 @@ function getTemplatePost(): string
 post;
 }
 
-$cwd = getcwd();
-$content_dir = "$cwd/content";
-$output_dir = "$cwd/output";
-
-checkEnv($cwd);
-checkDep($cwd);
-checkContent($content_dir);
-
-checkConfig($cwd, $default_config);
-checkTemplates($cwd . '/templates');
-
-$parser = new Parsedown();
-$config = require $cwd . '/mfconfig.php';
-
-rrmdir($output_dir);
-
-$page = 1;
-$list = [];
-$index = 0;
-$empty = true;
-
-foreach (getMarkdownFiles($content_dir) as $post) {
-    $empty === true and $empty = false;
-    $post['content'] = $parser->text($post['content']);
-    $post_rendered = render_template("$cwd/templates/post.php", ['post' => $post]);
-    $data = [
-        'body' => $post_rendered,
-        'config' => $config,
-    ];
-    $list[] = $post;
-
-    $page_rendered = render_template("$cwd/templates/layout.php", $data);
-    $post_output_dir = "$output_dir/$post[url]";
-    mkdir($post_output_dir, 0750, true);
-    file_put_contents("$post_output_dir/index.html", $page_rendered);
-
-    // File assets
-    if (is_dir($content_dir . $post['url'])) {
-        $assets = glob($content_dir . $post['url'] . '*');
-        foreach ($assets as $asset) {
-            copy($asset, $post_output_dir . '/' . basename($asset));
-        }
-    }
-
-    $this_is_the_end = !isset($post['next']);
-    if (++$index === 5 || $this_is_the_end) {
-        $index_rendered = render_template("$cwd/templates/index.php", [
-            'list' => $list,
-            'previous_url' => index_url($page - 1),
-            'next_url' => $this_is_the_end ? null : index_url($page + 1),
-        ]);
-        $data = [
-            'body' => $index_rendered,
-            'config' => $config,
-        ];
-        $page_rendered = render_template("$cwd/templates/layout.php", $data);
-        $page_slug = index_url($page);
-        $index_output_dir = "{$output_dir}{$page_slug}";
-
-        mkdirIfNotExists($index_output_dir, 0750, true);
-        file_put_contents("$index_output_dir/index.html", $page_rendered);
-
-        $index = 0;
-        $list = [];
-        $page++;
-    }
-}
-
-if ($empty) {
-    mkdirIfNotExists($output_dir);
-    $rendered = render_template("$cwd/templates/layout.php", compact('config'));
-    file_put_contents("$output_dir/index.html", $rendered);
-}
-
-$assets_dir = $content_dir . '/assets';
-if (is_dir($assets_dir)) {
-    $target_dir = $output_dir . '/assets';
-    $iterator_flags = FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_PATHNAME;
-    $iterator = new \RecursiveIteratorIterator(
-        new \RecursiveDirectoryIterator($assets_dir, $iterator_flags),
-        \RecursiveIteratorIterator::LEAVES_ONLY
-    );
-    foreach ($iterator as $asset) {
-        $relative_path = substr($asset, strlen($assets_dir) + 1);
-        $target_path = "$target_dir/$relative_path";
-        mkdirIfNotExists(dirname($target_path));
-        copy($asset, $target_path);
-    }
-}
+$generator = new MotherfuckingGenerator(getcwd());
+$generator->build();
 
 $motherfucking_time += hrtime(true);
 
